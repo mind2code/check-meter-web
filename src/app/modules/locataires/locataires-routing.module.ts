bf338bd8c4158623d4ce1a452db89bb730c35248
Ghislain ETIEN <ghislain.etien@outlook.com>
2023-01-08 18:12:59 +0000

    update avis-echeance

--- src/app/modules/locataires/locataires-routing.module.ts
+++ src/app/modules/locataires/locataires-routing.module.ts
@@ -3,11 +3,20 @@ import { RouterModule, Routes } from '@angular/router';
 import { LocatairesComponent } from './locataires.component';
 import {ContratsComponent} from "./contrats/contrats.component";
 import {CreerLocataireComponent} from "./creer-locataire/creer-locataire.component";
+import {LocataireDetailsComponent} from "./locataire-details/locataire-details.component";
+import {OverviewsComponent} from "./locataire-details/overviews/overviews.component";
+import {AvisEcheanceComponent} from "./locataire-details/avis-echeance/avis-echeance.component";
 
 const routes: Routes = [
   { path: '', component: LocatairesComponent },
   { path: 'contrats', component: ContratsComponent },
   { path: 'creer-locataire', component: CreerLocataireComponent },
+  { path: 'details',
+    component: LocataireDetailsComponent,
+    children: [
+      { path: 'overview', component: OverviewsComponent },
+      { path: 'avis-echeance', component: AvisEcheanceComponent },
+    ] },
 ];
 
 @NgModule({
