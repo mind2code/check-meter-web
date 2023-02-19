17911d76b59226ce7b85162e01bd28ceb431a181
Eugène YOBOUE <yaoeugeneyoboue@gmail.com>
2023-02-19 22:25:12 +0000

    Fix localize config

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -67,5 +67,5 @@ import 'zone.js';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
-
+import '@angular/localize/init';
 
